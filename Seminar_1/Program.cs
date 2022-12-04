25757e74349aff6945249ade78f999e238d5795f
ASFedotov <asfedotov1988as@gmail.com>
2022-12-05 01:34:11 +0500

    add task 1 max min

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Это число max = {number1}, это число min = {number2}");
+}
+else
+{
+    Console.WriteLine($"Это число max = {number2}, это число min = {number1}");
+}
