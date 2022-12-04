7b297b25cc8531747178eb2f3e804004ad2aefcf
ASFedotov <asfedotov1988as@gmail.com>
2022-12-05 01:47:20 +0500

    add task 2

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,5 +1,8 @@
-﻿//Задача 1
-Console.Write("Введите первое число: ");
+﻿// Задача 1: 
+// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+/*Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите второе число: ");
@@ -12,4 +15,23 @@ if (number1 > number2)
 else
 {
     Console.WriteLine($"Это число max = {number2}, это число min = {number1}");
-}
+}*/
+
+// Задача 2: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+/*Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+ if(num1 > max) max = num1;
+ if(num2 > max) max = num2;
+ if(num3 > max) max = num3;
+
+ Console.WriteLine($"Это максимальное число: {max}");*/
+
+ 
