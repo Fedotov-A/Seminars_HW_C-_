20382ae2f632824b95ac773826a65ab9e4f0d2d6
ASFedotov <asfedotov1988as@gmail.com>
2022-12-05 18:57:05 +0500

    add task 4

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -59,3 +59,18 @@ else
 {
     Console.WriteLine("Вы ввели не корректное значение");
 }*/
+
+// Задача 4: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+/*Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+int num = 0;
+
+while (count <= N)
+{ 
+    Console.WriteLine(num = num + 2);
+    count = count + 2;
+}*/
+
